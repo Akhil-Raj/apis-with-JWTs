1a1c93cf6fcb3084f9ce8bebb9e6dd0cd7460918
Akhil-Raj <akhil.raj1997@gmail.com>
2022-08-17 02:03:18 +0530

    v1

---
+++ db.js
@@ -0,0 +1,5 @@
+var mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://akhil:akhil@cluster0.hhipvvr.mongodb.net/?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
